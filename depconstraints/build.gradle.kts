b8352e2d11958f4bf7f71b03dda0230a8615c3c8
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 13:36:29 +0200

    Reformat code to match Kotlin official style

--- depconstraints/build.gradle.kts
+++ depconstraints/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  `java-platform`
-  `maven-publish`
+    `java-platform`
+    `maven-publish`
 }
 
 val resourceVersion = "0.14.1"
@@ -8,18 +8,18 @@ val checkerframeworkVersion = "3.16.0"
 val derive4jVersion = "1.1.1"
 
 dependencies {
-  constraints {
-    api("org.metaborg:resource:$resourceVersion")
-    api("org.checkerframework:checker-qual-android:$checkerframeworkVersion") // Use android version: annotation retention policy is class instead of runtime.
-    api("org.derive4j:derive4j:$derive4jVersion")
-    api("org.derive4j:derive4j-annotation:$derive4jVersion")
-  }
+    constraints {
+        api("org.metaborg:resource:$resourceVersion")
+        api("org.checkerframework:checker-qual-android:$checkerframeworkVersion") // Use android version: annotation retention policy is class instead of runtime.
+        api("org.derive4j:derive4j:$derive4jVersion")
+        api("org.derive4j:derive4j-annotation:$derive4jVersion")
+    }
 }
 
 publishing {
-  publications {
-    create<MavenPublication>("JavaPlatform") {
-      from(components["javaPlatform"])
+    publications {
+        create<MavenPublication>("JavaPlatform") {
+            from(components["javaPlatform"])
+        }
     }
-  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
 rootProject.name = "common.root"
 
 pluginManagement {
-  repositories {
-    maven("https://artifacts.metaborg.org/content/groups/public/")
-  }
+    repositories {
+        maven("https://artifacts.metaborg.org/content/groups/public/")
+    }
 }
 
 fun includeProject(path: String, id: String = "common.${path.replace('/', '.')}") {
-  include(id)
-  project(":$id").projectDir = file(path)
+    include(id)
+    project(":$id").projectDir = file(path)
 }
 
 include("common")
