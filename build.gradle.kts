e6d740b8ffce8fe37c8cd202589fce4c13657eae
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-16 21:05:07 +0200

    Syntax fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ allprojects {
     }
 
     repositories {
-        maven(url = "https://artifacts.metaborg.org/content/groups/public/")
+        maven("https://artifacts.metaborg.org/content/groups/public/")
         mavenCentral()
     }
 }
