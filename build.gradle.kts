1be9a315dc2a3dea63ca939b6beb6dbf0aa7e155
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-23 14:00:29 +0200

    Build and publish workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.metaborg.convention.Developer
 import org.metaborg.convention.MavenPublishConventionExtension
 
 // Workaround for issue: https://youtrack.jetbrains.com/issue/KTIJ-19369
@@ -7,8 +8,19 @@ plugins {
     alias(libs.plugins.gitonium)
 }
 
+rootProjectConvention {
+    // This will add `publishAll` and `publish` tasks that call the `publish` task on each subproject and sub-build
+    registerPublishTasks.set(true)
+}
+
 allprojects {
     apply(plugin = "org.metaborg.gitonium")
+
+    // Configure Gitonium before setting the version
+    gitonium {
+        mainBranch.set("master")
+    }
+
     version = gitonium.version
     group = "org.metaborg"
 
@@ -16,6 +28,14 @@ allprojects {
         extensions.configure(MavenPublishConventionExtension::class.java) {
             repoOwner.set("metaborg")
             repoName.set("common")
+
+            metadata {
+                inceptionYear.set("2018")
+                developers.set(listOf(
+                    Developer("gohla", "Gabriel Konat", "g.d.p.konat@tudelft.nl"),
+                    Developer("virtlink", "Daniel A. A. Pelsmaeker", "d.a.a.pelsmaeker@tudelft.nl"),
+                ))
+            }
         }
     }
 }
